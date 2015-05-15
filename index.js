1e406b45f1586268e058f634db501c493771e8e0
viktor <viktor@acklenavenue.com>
2015-05-15 01:45:23 -0600

    added index.js and initial structure and functionality. About to start working with mongoose

---
+++ index.js
@@ -0,0 +1,10 @@
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 8000 
+});
+
+server.start();
